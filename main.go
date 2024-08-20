9a6a21c695cd4e1434ab9da2406180d7ffc69dcd
Izaak Dale <izaakdaledev@gmail.com>
2024-08-19 22:33:02 -0700

    remove RootCA for testing lets encrypt

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"io"
 	"log"
 	"net/http"
@@ -17,26 +16,26 @@ func main() {
 		panic(err)
 	}
 
-	fCa, err := os.OpenFile(os.Getenv("ROOT_CA"), os.O_RDONLY, os.ModeTemporary)
-	if err != nil {
-		panic(err)
-	}
-	pemBytes, err := io.ReadAll(fCa)
-	if err != nil {
-		panic(err)
-	}
+	// fCa, err := os.OpenFile(os.Getenv("ROOT_CA"), os.O_RDONLY, os.ModeTemporary)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// pemBytes, err := io.ReadAll(fCa)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	caCertPool := x509.NewCertPool()
-	ok := caCertPool.AppendCertsFromPEM(pemBytes)
-	if !ok {
-		panic("error appending certs from pem")
-	}
+	// caCertPool := x509.NewCertPool()
+	// ok := caCertPool.AppendCertsFromPEM(pemBytes)
+	// if !ok {
+	// 	panic("error appending certs from pem")
+	// }
 
 	cli := http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
 				Certificates: []tls.Certificate{crt},
-				RootCAs:      caCertPool,
+				// RootCAs:      caCertPool,
 			},
 		},
 	}
